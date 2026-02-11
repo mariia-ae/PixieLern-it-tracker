b11b06b4c4d6d34ec47d0eabc5a1b993deea5484
MariiaCh21 <chertkovamasha21@icloud.com>
2026-02-11 14:33:08 +0100

    Initial fronted structure with dashboard and progress logic

---
+++ frontend_PL/js/dashboard.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    console.log("JS loaded");
+
+    const checkboxes = document.querySelectorAll('.topic-item input');
+    console.log("Checkbox count", checkboxes.length);
+    const progressText = document.querySelector('.progress-section p');
+    const progressFill = document.querySelector('.progress-fill');
+
+    function updateProgress() {
+        const total = checkboxes.length;
+        const checked = document.querySelectorAll('.topic-item input:checked').length;
+
+        const percent = Math.round((checked / total) * 100);
+        progressText.textContent = `Fortschritt: ${percent}%`;
+        progressFill.style.width = percent + "%";
+    }
+
+    checkboxes.forEach(box => {
+
+        box.addEventListener('change', updateProgress);
+
+    });
+});
+
