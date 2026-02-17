deefd3f57d1630ed7cb78f192a77cf8cb8c8d5f7
Мария Черткова <mariacertkova@MacBookPro.fritz.box>
2026-02-17 18:01:38 +0100

    Remove node_modules and fix gitigore

--- backend_PL/index.js
+++ backend_PL/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const db = new sqlite3.Database("./database.db", (err) =>{
     if (err) {
